14179a4d84651768791ebd6fba3f6e77d665d546
Luke Edwards <luke.edwards05@gmail.com>
2021-08-27 14:24:35 -0700

    chore: inline `worktop` internal utils

--- src/internal/utils.ts
+++ src/internal/utils.ts
@@ -1,19 +1,34 @@
-import { STATUS_CODES } from 'worktop';
-import { byteLength } from 'worktop/utils';
 import * as HEADERS from './headers';
+
+export const Encoder = /* @__PURE__ */ new TextEncoder();
+export const Decoder = /* @__PURE__ */ new TextDecoder();
+
+// Common error codes' status text
+export const STATUS_CODES: Record<string|number, string> = {
+	"400": "Bad Request",
+	"401": "Unauthorized",
+	"403": "Forbidden",
+	"404": "Not Found",
+	"405": "Method Not Allowed",
+	"411": "Length Required",
+	"413": "Payload Too Large",
+	"422": "Unprocessable Entity",
+	"426": "Upgrade Required",
+} as const;
 
 /**
  * @see https://github.com/lukeed/worktop/blob/3187246b95d50c7b34f987b95e734a1dbcf2d778/src/internal/ws.ts#L4
  */
 export function abort(code: number, message?: string) {
 	message = message || STATUS_CODES[code];
+	let length = Encoder.encode(message).byteLength;
 	return new Response(message, {
 		status: code,
 		statusText: STATUS_CODES[code],
 		headers: {
 			'Connection': 'close',
 			'Content-Type': 'text/plain',
-			'Content-Length': '' + byteLength(message)
+			'Content-Length': String(length)
 		}
 	});
 }
